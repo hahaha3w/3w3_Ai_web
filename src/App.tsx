e512ba240f1f1ffecb53fe3469d0a44a9bd3c003
yemomo <1463645455@qq.com>
2025-03-19 20:56:08 +0800

    init router

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className='w-300 h-100 bg-amber-50'>
+      <div className='w-300 h-100 bg-amber-50'>
       
-    </div>
+      </div>
   )
 }
 
