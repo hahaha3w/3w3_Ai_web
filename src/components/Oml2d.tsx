37adbb00efc7b16bd584eec7a951cd562434c8df
MengChen <tatsukimengchen@163.com>
2025-03-22 23:35:37 +0800

    feat(chat): 添加自定义滚动条样式并在消息为空时发送默认消息

--- src/components/Oml2d.tsx
+++ src/components/Oml2d.tsx
@@ -2,17 +2,26 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 
 import { CommonStyleType } from "node_modules/oh-my-live2d/dist/types/common";
-import { loadOml2d, Oml2dEvents, Oml2dMethods, Oml2dProperties } from "oh-my-live2d";
+import {
+  loadOml2d,
+  Oml2dEvents,
+  Oml2dMethods,
+  Oml2dProperties,
+} from "oh-my-live2d";
 import { memo, useEffect, useRef } from "react";
 import styles from "./Oml2d.module.scss";
+import useChatStore from "@/store/chat";
 
 type Oml2dProps = {
   oml2d: (Oml2dProperties & Oml2dMethods & Oml2dEvents) | null;
-  setOml2d: React.Dispatch<React.SetStateAction<(Oml2dProperties & Oml2dMethods & Oml2dEvents) | null>>;
-}
+  setOml2d: React.Dispatch<
+    React.SetStateAction<(Oml2dProperties & Oml2dMethods & Oml2dEvents) | null>
+  >;
+};
 
 const Oml2d = memo(({ oml2d, setOml2d }: Oml2dProps) => {
   const oml2dRef = useRef<HTMLDivElement>(null);
+  const { messages, addMessage } = useChatStore();
 
   const getPositionX = () => {
     if (window.innerWidth < 768) {
@@ -23,12 +32,27 @@ const Oml2d = memo(({ oml2d, setOml2d }: Oml2dProps) => {
 
   const getScale = () => {
     if (window.innerWidth < 768) {
-      const scale = oml2dRef.current ? oml2dRef.current.offsetWidth * 0.0005 : 0.2;
+      const scale = oml2dRef.current
+        ? oml2dRef.current.offsetWidth * 0.0005
+        : 0.2;
       return scale > 0.25 ? 0.25 : scale;
     }
-    const scale = oml2dRef.current ? oml2dRef.current.offsetHeight * 0.0005 : 0.2;
+    const scale = oml2dRef.current
+      ? oml2dRef.current.offsetHeight * 0.0005
+      : 0.2;
     return scale > 0.3 ? 0.3 : scale;
-  }
+  };
+
+  useEffect(() => {
+    if (messages.length === 0) {
+      addMessage({
+        key: Date.now(),
+        role: "ai",
+        content: "我是可爱的小猫咪，喵喵喵~",
+        timestamp: new Date(),
+      });
+    }
+  }, [messages]);
 
   useEffect(() => {
     const tipsStyle: CommonStyleType = {
@@ -41,24 +65,24 @@ const Oml2d = memo(({ oml2d, setOml2d }: Oml2dProps) => {
       boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)", // 添加阴影效果
       fontSize: "18px", // 设置字体大小
       textAlign: "center", // 文字居中对齐
-    }
+    };
     const stageStyle: CommonStyleType = {
       width: "100%",
-      height: "100%"
-    }
+      height: "100%",
+    };
     if (oml2d) {
       // @ts-ignore
       oml2d.options.tips!.style = {
         // @ts-ignore
         ...oml2d.options.tips!.style,
         ...tipsStyle,
-      }
+      };
       // @ts-ignore
       oml2d.options.tips!.mobileStyle = {
         // @ts-ignore
         ...oml2d.options.tips!.mobileStyle,
         ...tipsStyle,
-      }
+      };
       oml2d.reloadModel();
       return;
     }
@@ -75,15 +99,15 @@ const Oml2d = memo(({ oml2d, setOml2d }: Oml2dProps) => {
         disable: true,
       },
       stageStyle: {
-        ...stageStyle
+        ...stageStyle,
       },
       tips: {
         style: {
-          ...tipsStyle
+          ...tipsStyle,
         },
         mobileStyle: {
-          ...tipsStyle
-        }
+          ...tipsStyle,
+        },
       },
       transitionTime: 1000,
       models: [
@@ -127,33 +151,35 @@ const Oml2d = memo(({ oml2d, setOml2d }: Oml2dProps) => {
   }, [oml2d]);
 
   useEffect(() => {
-    oml2d?.tipsMessage("你才是猫娘，你全家都是猫娘！", 30000, 100);
+    // oml2d?.tipsMessage("你才是猫娘，你全家都是猫娘！", 30000, 100);
     oml2d?.onStageSlideIn(() => {
-      oml2d?.setModelPosition({ x: getPositionX() })
+      oml2d?.setModelPosition({ x: getPositionX() });
       oml2d?.setModelScale(getScale());
-    })
+    });
     oml2d?.onStageSlideOut(() => {
-      oml2d?.setModelPosition({ x: getPositionX() })
+      oml2d?.setModelPosition({ x: getPositionX() });
       oml2d?.setModelScale(getScale());
-    })
-  }, [oml2d])
+    });
+  }, [oml2d]);
 
   useEffect(() => {
-
     const handleResize = () => {
       // 直接更新模型位置和缩放
       oml2d?.setModelPosition({ x: getPositionX() });
       oml2d?.setModelScale(getScale());
-    }
-    window.addEventListener('resize', handleResize);
+    };
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, [oml2d]); // 添加依赖项
 
-  return <div
-    ref={oml2dRef}
-    className={`w-full bg-cover bg-center flex justify-center overflow-hidden pt-8 ${styles.container}`}></div>;
+  return (
+    <div
+      ref={oml2dRef}
+      className={`w-full bg-cover bg-center flex justify-center overflow-hidden pt-8 ${styles.container}`}
+    ></div>
+  );
 });
 
 export default Oml2d;
--- src/pages/chat/components/ChatArea.tsx
+++ src/pages/chat/components/ChatArea.tsx
@@ -1,149 +1,191 @@
-import Oml2d from '@/components/Oml2d';
-import { Bubble, Sender } from '@ant-design/x';
+import Oml2d from "@/components/Oml2d";
+import { Bubble, Sender } from "@ant-design/x";
 
-import { Icon } from '@iconify-icon/react/dist/iconify.js';
-import { ConfigProvider, Dropdown, GetProp, GetRef, MenuProps, theme, Tooltip } from 'antd';
-import { Oml2dEvents, Oml2dMethods, Oml2dProperties } from 'oh-my-live2d';
-import React, { useRef, useState } from 'react';
-import styles from './ChatArea.module.scss';
+import { Icon } from "@iconify-icon/react/dist/iconify.js";
+import {
+  ConfigProvider,
+  Dropdown,
+  GetProp,
+  GetRef,
+  MenuProps,
+  theme,
+  Tooltip,
+} from "antd";
+import { Oml2dEvents, Oml2dMethods, Oml2dProperties } from "oh-my-live2d";
+import React, { useRef, useState } from "react";
+import styles from "./ChatArea.module.scss";
+import useChatStore from "@/store/chat";
 
 type HeaderProps = {
-  title: string
+  title: string;
   oml2d: (Oml2dProperties & Oml2dMethods & Oml2dEvents) | null;
-}
+};
 
 const Header = ({ title, oml2d }: HeaderProps) => {
-  const items: MenuProps['items'] = [
+  const items: MenuProps["items"] = [
     {
-      key: '1',
+      key: "1",
       label: "切换模型",
       onClick: () => {
         if (oml2d) {
           oml2d.loadNextModel();
         }
-      }
+      },
     },
     {
-      key: '2',
+      key: "2",
       label: "你好，猫娘",
       onClick: () => {
-        oml2d?.tipsMessage("你才是猫娘，你全家都是猫娘！", 3000, 1);
-      }
+        oml2d?.tipsMessage("你才是猫娘，你全家都是猫娘！", 30000, 100);
+      },
     },
   ];
   return (
-    <div className='w-full h-[120px] absolute top-0 bg-gradient-to-b from-[rgba(0,0,0,0.2)] to-transparent z-10001 opacity-80'>
-      <div className='flex justify-between items-center h-[60px] px-4'>
-        <h2 className='text-2xl font-bold mt-0'>{title}</h2>
-        <div className='relative inline-block text-left'>
-          <Dropdown trigger={['click']} overlayClassName="chat-area-settings" menu={{ items }} placement="bottomRight" dropdownRender={(menus) => {
-            return (
-              <div className={styles.menu}>{menus}</div>
-            )
-          }}>
+    <div className="w-full h-[120px] absolute top-0 bg-gradient-to-b from-[rgba(0,0,0,0.2)] to-transparent z-10001 opacity-80">
+      <div className="flex justify-between items-center h-[60px] px-4">
+        <h2 className="text-2xl font-bold mt-0">{title}</h2>
+        <div className="relative inline-block text-left">
+          <Dropdown
+            trigger={["click"]}
+            overlayClassName="chat-area-settings"
+            menu={{ items }}
+            placement="bottomRight"
+            dropdownRender={(menus) => {
+              return <div className={styles.menu}>{menus}</div>;
+            }}
+          >
             <Icon icon="uil:setting" className="text-3xl" />
           </Dropdown>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-const rolesAsObject: GetProp<typeof Bubble.List, 'roles'> = {
+const rolesAsObject: GetProp<typeof Bubble.List, "roles"> = {
   ai: {
-    placement: 'start',
+    placement: "start",
     typing: { step: 5, interval: 20 },
     style: {
       maxWidth: 600,
     },
-    shape: 'round',
+    shape: "round",
     styles: {
       content: {
-        color: 'white',
-        background: 'rgba(0,0,0,0.5)',
-      }
+        color: "white",
+        background: "rgba(0,0,0,0.5)",
+      },
     },
   },
   user: {
-    placement: 'end',
-    shape: 'round',
+    placement: "end",
+    shape: "round",
     styles: {
       content: {
-        color: 'white',
-        background: 'rgba(0,0,0,0.5)',
-      }
+        color: "white",
+        background: "rgba(0,0,0,0.5)",
+      },
     },
   },
 };
 
-const MessageList: React.FC = () => {
+type MessageListProps = {
+  oml2d: (Oml2dProperties & Oml2dMethods & Oml2dEvents) | null;
+};
+
+const MessageList: React.FC<MessageListProps> = ({ oml2d }) => {
   const listRef = useRef<GetRef<typeof Bubble.List>>(null);
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
   const [loading, setLoading] = useState(false);
+  const { messages, addMessage } = useChatStore();
+  const [lastAiMessage, setLastAiMessage] = useState<string | null>(null);
+
+  const handleSend = (message: string) => {
+    addMessage({
+      key: Date.now(),
+      role: "user",
+      content: message,
+      timestamp: new Date(),
+    });
+    setTimeout(() => {
+      setLoading(false);
+      addMessage({
+        key: Date.now(),
+        role: "ai",
+        content: "你才是猫娘，你全家都是猫娘！",
+        timestamp: new Date(),
+      });
+      oml2d?.tipsMessage("你才是猫娘，你全家都是猫娘！", 300, 1);
+    }, 3000);
+  };
 
   return (
-    <div className='w-full h-full box-border flex justify-end flex-col top-0 absolute p-4  z-10000'>
+    <div className="w-full h-full box-border flex justify-end flex-col top-0 absolute p-4  z-10000">
       <Bubble.List
+        className={styles.bubbleListContainer}
         ref={listRef}
-        style={{ maxHeight: 300 }}
         roles={rolesAsObject}
-        items={[
-          { key: 1, role: 'ai', content: "我是可爱的小猫咪，喵喵喵~" },
-          { key: 1, role: 'user', content: "你好，猫娘" },
-          { key: 1, role: 'ai', content: "你才是猫娘，你全家都是猫娘！" },
-        ]}
+        items={messages}
       />
       <Sender
+        className="mt-4 bg-[rgba(0,0,0,0.5)]"
         value={value}
         onChange={setValue}
         loading={loading}
-        onSubmit={() => {
-          setValue('');
+        onSubmit={(value) => {
+          setValue("");
           setLoading(true);
+          handleSend(value);
         }}
-        placeholder='聊点什么呢？'
+        placeholder="聊点什么呢？"
         onCancel={() => {
           setLoading(false);
         }}
         actions={(_, info) => {
-          const { SendButton } = info.components;
+          const { SendButton, LoadingButton } = info.components;
 
           if (loading) {
             return (
-              <Tooltip title="Sending...">
-                <SendButton />
+              <Tooltip title="取消">
+                <LoadingButton />
               </Tooltip>
             );
           }
 
           return (
-            <Tooltip title={value ? 'Send \u21B5' : 'Please type something'}>
-              <SendButton />
+            <Tooltip title={value ? "发送 \u21B5" : "请输入内容"}>
+              <SendButton
+                className={`${styles.sendButton}`}
+                variant="text"
+                icon={<Icon icon="iconamoon:send-light" />}
+              />
             </Tooltip>
           );
         }}
       />
     </div>
-  )
-}
+  );
+};
 
 type ChatAreaProps = {
-  title: string
-}
+  title: string;
+};
 
 const ChatArea = ({ title }: ChatAreaProps) => {
-  const [oml2d, setOml2d] = useState<(Oml2dProperties & Oml2dMethods & Oml2dEvents) | null>(null);
+  const [oml2d, setOml2d] = useState<
+    (Oml2dProperties & Oml2dMethods & Oml2dEvents) | null
+  >(null);
 
   return (
     <ConfigProvider theme={{ algorithm: theme.darkAlgorithm }}>
-      <div className='relative text-gray-50'>
+      <div className="relative text-gray-50">
         {/* 修正：正确传递 className 属性 */}
         <Header title={title} oml2d={oml2d} />
         <Oml2d oml2d={oml2d} setOml2d={setOml2d} />
-        <MessageList />
+        <MessageList oml2d={oml2d} />
       </div>
     </ConfigProvider>
-  )
-}
+  );
+};
 
-export default ChatArea
+export default ChatArea;
